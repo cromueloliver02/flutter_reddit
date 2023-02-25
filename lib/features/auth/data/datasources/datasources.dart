e1c5d12693e56c13cf41b4276580feca239494b7
Cromuel Oliver <cromuelbarut@gmail.com>
2023-02-25 20:40:29 +0800

    created google sign in datasource

---
+++ lib/features/auth/data/datasources/datasources.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart' as fb_auth;
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+abstract class GoogleSignInDataSource {
+  Future<fb_auth.OAuthCredential> getUserCredential();
+}
+
+class GoogleSignInDataSourceImpl extends GoogleSignInDataSource {
+  final GoogleSignIn _googleSignIn;
+
+  GoogleSignInDataSourceImpl({
+    required GoogleSignIn googleSignIn,
+  }) : _googleSignIn = googleSignIn;
+
+  @override
+  Future<fb_auth.OAuthCredential> getUserCredential() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      // if (googleAuth == null) return;
+
+      final fb_auth.OAuthCredential oAuthCredential =
+          fb_auth.GoogleAuthProvider.credential(
+        idToken: googleAuth?.idToken,
+        accessToken: googleAuth?.accessToken,
+      );
+
+      return oAuthCredential;
+    } catch (err) {
+      debugPrint(err.toString());
+      rethrow;
+    }
+  }
+}
