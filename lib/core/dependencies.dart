33b7561d12984b2f01e8395780fa2ca76286f63b
Cromuel Oliver <cromuelbarut@gmail.com>
2023-02-25 13:02:48 +0800

    setup dependency injector

---
+++ lib/core/dependencies.dart
@@ -0,0 +1,20 @@
+import 'package:get_it/get_it.dart';
+
+// service locator
+final GetIt sl = GetIt.instance;
+
+void setup() {
+  // external services
+
+  // data sources
+
+  // repositories
+
+  // use cases
+
+  // blocs
+
+  // cubits
+
+  // utilities
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'core/dependencies.dart' as di;
 import 'reddit_app.dart';
 
 void main() {
+  di.setup();
+
   runApp(const RedditApp());
 }
