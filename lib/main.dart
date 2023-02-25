469cab9ca6148a31246fec9e95c3df7e5e6038d9
Cromuel Oliver <cromuelbarut@gmail.com>
2023-02-25 15:38:09 +0800

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'core/dependencies.dart' as di;
+import 'firebase_options.dart';
 import 'reddit_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   di.setup();
 
   runApp(const RedditApp());
