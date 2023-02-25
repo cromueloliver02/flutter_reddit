e6cdb510b7724c0f6042bca6fbb75531f70ea78f
Cromuel Oliver <cromuelbarut@gmail.com>
2023-02-26 01:16:57 +0800

    injected newly created dependencies

--- lib/core/dependencies.dart
+++ lib/core/dependencies.dart
@@ -1,20 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import '../features/auth/data/datasources/datasources.dart';
+import '../features/auth/data/repositories/repositories.dart';
+import '../features/auth/domain/repositories/repositories.dart';
+import '../features/auth/domain/usecases/usecases.dart';
+import '../features/auth/presentation/cubits/cubits.dart';
 
 // service locator
 final GetIt sl = GetIt.instance;
 
 void setup() {
   // external services
+  sl.registerLazySingleton<GoogleSignIn>(() => GoogleSignIn());
+  sl.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+  sl.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
 
   // data sources
+  sl.registerLazySingleton<GoogleSignInDataSource>(
+    () => GoogleSignInDataSourceImpl(googleSignIn: sl<GoogleSignIn>()),
+  );
+  sl.registerLazySingleton<FirebaseAuthDataSource>(
+    () => FirebaseAuthDataSourceImpl(firebaseAuth: sl<FirebaseAuth>()),
+  );
+  sl.registerLazySingleton<UserRemoteDataSource>(
+    () => UserRemoteDataSourceImpl(firestore: sl<FirebaseFirestore>()),
+  );
 
   // repositories
+  sl.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(
+        googleSignInDataSource: sl<GoogleSignInDataSource>(),
+        firebaseAuthDataSource: sl<FirebaseAuthDataSource>(),
+        userRemoteDataSource: sl<UserRemoteDataSource>(),
+      ));
 
   // use cases
+  sl.registerLazySingleton<LoginWithGoogle>(
+    () => LoginWithGoogle(authRepository: sl<AuthRepository>()),
+  );
 
   // blocs
 
   // cubits
+  sl.registerFactory<SignInCubit>(
+    () => SignInCubit(loginWithGoogle: sl<LoginWithGoogle>()),
+  );
 
   // utilities
 }
