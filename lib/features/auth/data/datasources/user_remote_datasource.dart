18f535137ef921118a62456f92d7703192569552
Cromuel Oliver <cromuelbarut@gmail.com>
2023-02-27 21:51:29 +0800

    created get user by id method of user remote datasource

--- lib/features/auth/data/datasources/user_remote_datasource.dart
+++ lib/features/auth/data/datasources/user_remote_datasource.dart
@@ -2,9 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../../../../core/constants/firebase_constants.dart';
 import '../../../../core/errors/exceptions/exceptions.dart';
+import '../../domain/entities/entities.dart';
 import '../models/models.dart';
 
 abstract class UserRemoteDataSource {
+  Future<User> getById(String userId);
   Future<void> post(UserModel user);
 }
 
@@ -14,6 +16,22 @@ class UserRemoteDataSourceImpl implements UserRemoteDataSource {
   const UserRemoteDataSourceImpl({
     required FirebaseFirestore firestore,
   }) : _firestore = firestore;
+
+  @override
+  Future<User> getById(String userId) async {
+    try {
+      final DocumentSnapshot userDoc =
+          await _firestore.collection(kUsersCollection).doc(userId).get();
+
+      final User user = UserModel.fromDoc(userDoc);
+
+      return user;
+    } on FirebaseException catch (err, stackTrace) {
+      throw ServerException(error: err, stackTrace: stackTrace);
+    } catch (err, stackTrace) {
+      throw UnexpectedException(error: err, stackTrace: stackTrace);
+    }
+  }
 
   @override
   Future<void> post(UserModel user) async {
