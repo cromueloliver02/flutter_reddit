1a7efd768f91b2989b3de7102e6ac48b7de36552
Cromuel Oliver <cromuelbarut@gmail.com>
2023-03-11 19:13:10 +0800

    refatored community repository impl

--- lib/features/community/data/repositories/community_repository_impl.dart
+++ lib/features/community/data/repositories/community_repository_impl.dart
@@ -16,6 +16,24 @@ class CommunityRepositoryImpl implements CommunityRepository {
   }) : _communityRemoteDataSource = communityRemoteDataSource;
 
   @override
+  Either<Failure, Stream<List<Community>>> getUserCommunities(String userId) {
+    try {
+      final Stream<List<Community>> communitiesStream =
+          _communityRemoteDataSource.getAllByUserId(userId);
+
+      return Right(communitiesStream);
+    } on ServerException catch (err) {
+      return Left(ServerFailure(exception: err));
+    } on NetworkException catch (err) {
+      return Left(NetworkFailure(exception: err));
+    } on UnexpectedException catch (err) {
+      return Left(UnexpectedFailure(exception: err));
+    } catch (err) {
+      return Left(UnexpectedFailure(exception: err));
+    }
+  }
+
+  @override
   FutureEitherVoid createCommunity(CommunityModel community) async {
     try {
       final Community? existingCommunity =
@@ -30,24 +48,6 @@ class CommunityRepositoryImpl implements CommunityRepository {
       return const Right(null);
     } on CommunityNameAlreadyExistException {
       return Left(CommunityNameAlreadyExistFailure());
-    } on ServerException catch (err) {
-      return Left(ServerFailure(exception: err));
-    } on NetworkException catch (err) {
-      return Left(NetworkFailure(exception: err));
-    } on UnexpectedException catch (err) {
-      return Left(UnexpectedFailure(exception: err));
-    } catch (err) {
-      return Left(UnexpectedFailure(exception: err));
-    }
-  }
-
-  @override
-  Either<Failure, Stream<List<Community>>> getUserCommunities(String userId) {
-    try {
-      final Stream<List<Community>> communitiesStream =
-          _communityRemoteDataSource.getAllByUserId(userId);
-
-      return Right(communitiesStream);
     } on ServerException catch (err) {
       return Left(ServerFailure(exception: err));
     } on NetworkException catch (err) {
