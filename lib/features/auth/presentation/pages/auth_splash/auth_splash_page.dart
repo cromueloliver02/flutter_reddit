2b2b88d3f13e4ac23f998d5d692bf50e232c3928
Cromuel Oliver <cromuelbarut@gmail.com>
2023-03-11 15:22:36 +0800

    created error handler for authentication

--- lib/features/auth/presentation/pages/auth_splash/auth_splash_page.dart
+++ lib/features/auth/presentation/pages/auth_splash/auth_splash_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../core/blocs/blocs.dart';
+import '../../../../../core/utils/utils.dart';
 import '../../blocs/blocs.dart';
 import 'components/auth_splash_view.dart';
 
@@ -16,8 +17,19 @@ class AuthSplashPage extends StatefulWidget {
 }
 
 class _AuthSplashPageState extends State<AuthSplashPage> {
+  void _authListener(BuildContext ctx, AuthState state) {
+    if (state.status == AuthStatus.failure) {
+      showErrorDialog(ctx, message: state.error.message);
+    }
+  }
+
   @override
-  Widget build(BuildContext context) => const AuthSplashView();
+  Widget build(BuildContext context) {
+    return BlocListener<AuthBlocImpl, AuthState>(
+      listener: _authListener,
+      child: const AuthSplashView(),
+    );
+  }
 
   // start authentication
   @override
