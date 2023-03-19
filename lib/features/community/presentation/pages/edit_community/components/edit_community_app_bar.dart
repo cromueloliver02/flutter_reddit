5c55eea79767a8a1eadb28468aa6b98c4293f899
Cromuel Oliver <cromuelbarut@gmail.com>
2023-03-19 20:50:59 +0800

    created app bar of edit community page

---
+++ lib/features/community/presentation/pages/edit_community/components/edit_community_app_bar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EditCommunityAppBar extends StatelessWidget {
+  const EditCommunityAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text('Edit Community'),
+      centerTitle: true,
+    );
+  }
+}
--- lib/features/community/presentation/pages/edit_community/components/edit_community_view.dart
+++ lib/features/community/presentation/pages/edit_community/components/edit_community_view.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'edit_community_app_bar.dart';
+
 class EditCommunityView extends StatelessWidget {
   const EditCommunityView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: EditCommunityAppBar(),
+      ),
       body: Center(
         child: Text('EDIT COMMUNITY PAGE'),
       ),
