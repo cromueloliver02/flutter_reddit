a34613c1601e06d8adbb464e2e4afa617d4c7743
Cromuel Oliver <cromuelbarut@gmail.com>
2023-03-20 23:40:25 +0800

    created search community delegate

--- lib/features/home/presentation/pages/home/components/home_app_bar.dart
+++ lib/features/home/presentation/pages/home/components/home_app_bar.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../../core/blocs/blocs.dart';
+import '../delegates/search_community_delegate.dart';
 
 class HomeAppBar extends StatelessWidget {
   const HomeAppBar({super.key});
 
   void _openDrawer(BuildContext ctx) => Scaffold.of(ctx).openDrawer();
+
+  void _showSearch(BuildContext ctx) {
+    showSearch(context: ctx, delegate: SearchCommunityDelegate());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +24,7 @@ class HomeAppBar extends StatelessWidget {
       ),
       actions: [
         IconButton(
-          onPressed: () {},
+          onPressed: () => _showSearch(context),
           icon: const Icon(Icons.search),
         ),
         IconButton(
---
+++ lib/features/home/presentation/pages/home/delegates/search_community_delegate.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SearchCommunityDelegate extends SearchDelegate {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () => query = '',
+        icon: const Icon(Icons.close),
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return null;
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
