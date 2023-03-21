0572155646a8c866229ddec6d7524133fa436ce1
Cromuel Oliver <cromuelbarut@gmail.com>
2023-03-21 22:42:27 +0800

    completed UI of profile drawer

--- lib/features/home/presentation/pages/home/components/profile_drawer.dart
+++ lib/features/home/presentation/pages/home/components/profile_drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../../core/blocs/blocs.dart';
+import '../../../../../../core/utils/utils.dart';
 import '../../../../../auth/domain/entities/entities.dart';
 
 class ProfileDrawer extends StatelessWidget {
@@ -9,13 +10,47 @@ class ProfileDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
     final User user = context.watch<AuthBloc>().state.user!;
 
     return Drawer(
       child: SafeArea(
         child: Column(
           children: [
-            CircleAvatar(backgroundImage: NetworkImage(user.profilePic)),
+            CircleAvatar(
+              backgroundImage: NetworkImage(user.profilePic),
+              radius: 70,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'u/${user.name}',
+              style: textTheme.bodyLarge!.copyWith(
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Divider(),
+            ListTile(
+              title: const Text('My Profile'),
+              leading: const Icon(Icons.person),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Logout'),
+              leading: Icon(
+                Icons.logout,
+                color: AppTheme.redColor,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Dark Mode'),
+              leading: const Icon(Icons.dark_mode),
+              trailing: Switch.adaptive(
+                value: true,
+                onChanged: (bool value) {},
+              ),
+            ),
           ],
         ),
       ),
